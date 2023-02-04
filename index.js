6fa5bdcbc7c42ca5f357636d034df88bbed108f1
HovhannisyanAram <aramoffpage@gmail.com>
2023-02-04 20:05:40 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const WINNING_SUM = 123.40;
 function doesCartWinPrize(cartItemsPrices) {
   let sum = cartItemsPrices.reduce((a, b) => parseFloat(a) + parseFloat(b), 0).toFixed(2);
   if(+sum === WINNING_SUM) {
-    return true
+   return true
   } else { return false };
 }
 
